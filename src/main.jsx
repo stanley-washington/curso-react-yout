3016d274968b5db9fdecb0524618ae9542e1660d
Stanley Washington Ferreira de Rezende <stanley_washington@ufu.br>
2024-12-24 16:50:06 -0300

    Git Inicial

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/*<App />*/}
     <RouterProvider router={router} />
   </StrictMode>
 );
